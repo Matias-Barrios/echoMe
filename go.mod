d2258162c546d6e4d7ff8e9746313424f54eeda8
Matias Barrios <GCabrera@humana.com>
2020-08-21 23:10:20 -0300

    master

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Matias-Barrios/echoMe
+
+go 1.13
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,47 +3,58 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/Matias-Barrios/echoMe/models"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
+
+	"github.com/Matias-Barrios/echoMe/models"
 )
 
 func PlainTextEcho(w http.ResponseWriter, r *http.Request) {
+	var writers []io.Writer
+	writers = append(writers, os.Stdout)
+	writers = append(writers, w)
+	dest := io.MultiWriter(writers...)
 	if r.URL.Path != "/" {
-        errorHandler(w, r, http.StatusNotFound)
-        return
-    }
-	payload, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Fprintf(w, err.Error())
+		errorHandler(w, r, http.StatusNotFound)
 		return
 	}
-	fmt.Fprintf(w, "Method -> \n")
-	fmt.Fprintf(w, "\t%s\n\n", r.Method)
-	fmt.Fprintf(w, "Protocol -> \n")
-	fmt.Fprintf(w, "\t%s\n\n", r.Proto)
-	fmt.Fprintf(w, "Headers -> \n")
-	for k, v := range r.Header {
-		fmt.Fprintf(w, "\t%s: %s\n", k, v)
+	payload, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(dest, err.Error())
+		return
 	}
-	fmt.Fprintf(w, "\nContent length (in Bytes) -> \n")
-	fmt.Fprintf(w, "\t%d\n\n", r.ContentLength)
-	fmt.Fprintf(w, "\nRemote address -> \n")
-	fmt.Fprintf(w, "\t%s\n", r.RemoteAddr)
-	fmt.Fprintf(w, "\nPayload\n")
-	fmt.Fprintf(w, "####################\n")
-	fmt.Fprintf(w, "%s\n", payload)
-	fmt.Fprintf(w, "####################\n")
+	fmt.Fprintf(dest, "Method -> \n")
+	fmt.Fprintf(dest, "\t%s\n\n", r.Method)
+	fmt.Fprintf(dest, "Protocol -> \n")
+	fmt.Fprintf(dest, "\t%s\n\n", r.Proto)
+	fmt.Fprintf(dest, "Headers -> \n")
+	for k, v := range r.Header {
+		fmt.Fprintf(dest, "\t%s: %s\n", k, v)
+	}
+	fmt.Fprintf(dest, "\nContent length (in Bytes) -> \n")
+	fmt.Fprintf(dest, "\t%d\n\n", r.ContentLength)
+	fmt.Fprintf(dest, "\nRemote address -> \n")
+	fmt.Fprintf(dest, "\t%s\n", r.RemoteAddr)
+	fmt.Fprintf(dest, "\nPayload\n")
+	fmt.Fprintf(dest, "####################\n")
+	fmt.Fprintf(dest, "%s\n", payload)
+	fmt.Fprintf(dest, "####################\n")
 }
 
 func JsonEcho(w http.ResponseWriter, r *http.Request) {
+	var writers []io.Writer
+	writers = append(writers, os.Stdout)
+	writers = append(writers, w)
+	dest := io.MultiWriter(writers...)
 	if r.URL.Path != "/json" {
-        errorHandler(w, r, http.StatusNotFound)
-        return
-    }
+		errorHandler(w, r, http.StatusNotFound)
+		return
+	}
 	payload, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		fmt.Fprintf(w, err.Error())
+		fmt.Fprintf(dest, err.Error())
 		return
 	}
 
@@ -60,10 +71,10 @@ func JsonEcho(w http.ResponseWriter, r *http.Request) {
 	}
 	jsonbody, err := json.MarshalIndent(resp, "", "    ")
 	if err != nil {
-		fmt.Fprintf(w, err.Error())
+		fmt.Fprintf(dest, err.Error())
 		return
 	}
-	fmt.Fprintf(w, string(jsonbody)+"\n")
+	fmt.Fprintf(dest, string(jsonbody)+"\n")
 }
 
 func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
